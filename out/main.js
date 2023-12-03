270881a19411e29e4ccddf0d73b1996c064b8a34
Zhivko <zivkostoimcev1551@outlook.com>
2023-12-04 00:53:02 +0100

    removed files from ./out

--- out/main.js
+++
@@ -1,8 +0,0 @@
-import { initGfx, setDrawables } from "./gfx.js";
-import { generatePoints } from "./data.js";
-function main() {
-    initGfx();
-    const points = generatePoints(50);
-    setDrawables(points);
-}
-window.onload = main; //last line in the file, main function is initial
